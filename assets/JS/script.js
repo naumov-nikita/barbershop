8f098d68dd4c5fc8310a08ed5c70967a3616c274
nick <nan99.rnd@gmail.com>
2021-12-07 19:37:15 +0300

    Added popups opening functionality

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,84 @@
-'use	strict';
+'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const modalMap = document.querySelector('.modal-map'),
+    modalLogin = document.querySelector('.modal-login'),
+    mapCloseBtn = modalMap.querySelector('.modal__close-btn'),
+    loginCloseBtn = modalLogin.querySelector('.modal__close-btn'),
+    loginTrigger = document.querySelector('.header__login-link'),
+    mapTriggerFooter = document.querySelector('.map-link'),
+    login = modalLogin.querySelector('#user-email'),
+    password = modalLogin.querySelector('#user-password'),
+    loginForm = modalLogin.querySelector('form'),
+    mapTriggerInfo = document.querySelector('[data-mapTrigger]');
 
-	const links = document.querySelectorAll('a');
+  let storage = '',
+    isStorageSupport = true;
 
-	links.forEach((link) => {
-		link.addEventListener("click",(e) => {
-		e.preventDefault();
+  try {
+    storage = localStorage.getItem('user-login');
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
-		});
-	});
+  function openModalLogin(triggerBtn, modalWin) {
+    triggerBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      modalWin.classList.remove('is-hide');
+      if (storage) {
+        login.value = storage;
+        password.focus();
+      } else {
+        login.focus();
+      }
+    });
+  }
+  openModalLogin(loginTrigger, modalLogin);
 
+  function openModalMap(triggerBtn, modalWin) {
+    triggerBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      modalWin.classList.remove('is-hide');
+    });
+  }
+  openModalMap(mapTriggerFooter, modalMap);
+  openModalMap(mapTriggerInfo, modalMap);
+
+  function closeModal(parrentElement, closeBtn) {
+    closeBtn.addEventListener('click', () => {
+      parrentElement.classList.remove('is-hide');
+      modalLogin.classList.remove('modal-error');
+    });
+  }
+
+  closeModal(modalMap, mapCloseBtn);
+  closeModal(modalLogin, loginCloseBtn);
+
+  loginForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if (!password.value || !login.value) {
+      modalLogin.classList.remove('modal-error');
+      void modalLogin.offsetWidth;
+      modalLogin.classList.add('modal-error');
+			console.log(modalLogin.offsetWidth);
+    } else if (isStorageSupport) {
+      localStorage.setItem('user-login', login.value);
+      alert('Вы вошли. Ваш логин сохранен');
+    } else {
+      alert('Вы успешно вошли');
+    }
+  });
+
+  window.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) {
+      if (!modalLogin.classList.contains('is-hide')) {
+        evt.preventDefault();
+        modalLogin.classList.add('is-hide');
+        modalLogin.classList.remove('modal-error');
+      } else if (!modalMap.classList.contains('is-hide')) {
+        evt.preventDefault();
+        modalMap.classList.add('is-hide');
+      }
+    }
+  });
 });
